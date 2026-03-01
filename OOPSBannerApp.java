d2f096862c938a265bea84a689b7b9c84b065c99
jagvijay singh <akshanshsingh2007@gmail.com>
2026-03-01 21:51:04 +0530

    uc5

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,20 +1,18 @@
-public class OOPSBanner {
+public class OOPSBannerApp {
     public static void main(String[] args) {
 
-        // String array to store all 7 lines
+
         String[] banner = {
-
-            String.join(" ", "  *****  ", " *****  ", " ****** ", " ****** "),
-            String.join(" ", " *     * ", "*     * ", "*     * ", "*     * "),
-            String.join(" ", " *     * ", "*     * ", "*     * ", "*       "),
-            String.join(" ", " *     * ", "*     * ", " ****** ", " ****** "),
-            String.join(" ", " *     * ", "*     * ", "*       ", "       *"),
-            String.join(" ", " *     * ", "*     * ", "*       ", "*     * "),
-            String.join(" ", "  *****  ", " *****  ", "*       ", " ****** ")
-
+            String.join(" ", "  *****  ", " *****   ", " ******  ", " ******  "),
+            String.join(" ", " *     * ", "*     *  ", "*     *  ", "*     *  "),
+            String.join(" ", " *     * ", "*     *  ", "*     *  ", "*        "),
+            String.join(" ", " *     * ", "*     *  ", " ******  ", " ******  "),
+            String.join(" ", " *     * ", "*     *  ", "*        ", "       * "),
+            String.join(" ", " *     * ", "*     *  ", "*        ", "*     *  "),
+            String.join(" ", "  *****  ", " *****   ", "*        ", " ******  ")
         };
 
-        // for-each loop to print banner
+
         for (String line : banner) {
             System.out.println(line);
         }
