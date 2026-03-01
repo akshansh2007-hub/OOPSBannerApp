517d9f9f62e722caf4afe89ad9f785e37c161ef2
jagvijay singh <akshanshsingh2007@gmail.com>
2026-03-01 21:38:43 +0530

    uc3

---
+++ OOPSBannerApp.java
@@ -0,0 +1,14 @@
+public class OOPSBannerApp {
+    public static void main(String[] args) {
+
+        // OOPS Banner
+        System.out.println(String.join(" ", " *** ", " *** ", " ****", " ****"));
+        System.out.println(String.join(" ", "*   *", "*   *", "*   *", "*   *"));
+        System.out.println(String.join(" ", "*   *", "*   *", "*   *", "*    "));
+        System.out.println(String.join(" ", "*   *", "*   *", " ****", " *** "));
+        System.out.println(String.join(" ", "*   *", "*   *", "*    ", "    *"));
+        System.out.println(String.join(" ", "*   *", "*   *", "*    ", "*   *"));
+        System.out.println(String.join(" ", " *** ", " *** ", "*    ", " *** "));
+
+    }
+}
