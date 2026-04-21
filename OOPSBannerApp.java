9d076487b4622ec0abc235ff69b062b9ca993026
jagvijay singh <akshanshsingh2007@gmail.com>
2026-04-21 16:11:21 +0530

    Vuc2

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,13 +1,35 @@
-import java.util.Scanner;
+import java.util.Random;
 
 class OOPSBannerApp {
-    static int getUserSlot() {
-        Scanner sc = new Scanner(System.in);
-        return sc.nextInt();
+    static String player1;
+    static String player2;
+    static char player1Symbol;
+    static char player2Symbol;
+    static String currentPlayer;
+
+    static void toss() {
+        Random random = new Random();
+        boolean player1Starts = random.nextBoolean();
+
+        if (player1Starts) {
+            currentPlayer = player1;
+            player1Symbol = 'X';
+            player2Symbol = 'O';
+        } else {
+            currentPlayer = player2;
+            player1Symbol = 'O';
+            player2Symbol = 'X';
+        }
     }
 
     public static void main(String[] args) {
-        int slot = getUserSlot();
-        System.out.println(slot);
+        player1 = "Player1";
+        player2 = "Player2";
+
+        toss();
+
+        System.out.println("First turn: " + currentPlayer);
+        System.out.println("Player1 symbol: " + player1Symbol);
+        System.out.println("Player2 symbol: " + player2Symbol);
     }
 }
