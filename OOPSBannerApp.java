631284c7bef114b555b09f2687cc1067d8a7bbce
jagvijay singh <akshanshsingh2007@gmail.com>
2026-04-21 15:41:38 +0530

    IVuc2

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,10 +1,10 @@
 public class OOPSBannerApp {
-    public void printMessage() {
-        System.out.println("Hello World");
-    }
-
     public static void main(String[] args) {
-        OOPSBannerApp app = new OOPSBannerApp();
-        app.printMessage();
+        if (args.length > 0) {
+            String name = args[0];
+            System.out.println("Hello, " + name + "!");
+        } else {
+            System.out.println("Please provide a name as a command-line argument.");
+        }
     }
 }
