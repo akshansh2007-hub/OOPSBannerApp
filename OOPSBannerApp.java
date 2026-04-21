4cac7f0121c77e1f7e426fd5f643e7128cbbb44a
jagvijay singh <akshanshsingh2007@gmail.com>
2026-04-21 16:04:51 +0530

    Vuc6

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,33 +1,44 @@
-import java.util.Random;
+import java.util.Scanner;
 
-public class OOPSBannerApp {
-    static char[] board = {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
+class OOPSBannerApp {
+    static char[][] board = {
+        {'-', '-', '-'},
+        {'-', '-', '-'},
+        {'-', '-', '-'}
+    };
+
+    static void displayBoard() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    static boolean updateBoard(int row, int col, char symbol) {
+        if (row >= 0 && row < 3 && col >= 0 && col < 3 && board[row][col] == '-') {
+            board[row][col] = symbol;
+            return true;
+        }
+        return false;
+    }
 
     public static void main(String[] args) {
-        computerMove();
-        printBoard();
-    }
+        Scanner sc = new Scanner(System.in);
+        displayBoard();
+        System.out.print("Enter row and column: ");
+        int row = sc.nextInt();
+        int col = sc.nextInt();
+        System.out.print("Enter symbol (X/O): ");
+        char symbol = sc.next().charAt(0);
 
-    static void computerMove() {
-        Random random = new Random();
-        int position;
-
-        do {
-            int slot = random.nextInt(9) + 1;
-            position = slot - 1;
-        } while (board[position] != ' ');
-
-        board[position] = 'O';
-    }
-
-    static void printBoard() {
-        for (int i = 0; i < board.length; i++) {
-            System.out.print(board[i]);
-            if ((i + 1) % 3 == 0) {
-                System.out.println();
-            } else {
-                System.out.print(" | ");
-            }
+        if (updateBoard(row, col, symbol)) {
+            System.out.println("Board updated:");
+        } else {
+            System.out.println("Invalid move");
         }
+
+        displayBoard();
     }
 }
