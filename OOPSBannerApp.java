e71836ab184877a1915130303f6e88e745eea8c6
jagvijay singh <akshanshsingh2007@gmail.com>
2026-04-21 16:01:34 +0530

    Vuc1

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,10 +1,33 @@
+import java.util.Random;
+
 public class OOPSBannerApp {
+    static char[] board = {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
+
     public static void main(String[] args) {
-        if (args.length == 0) {
-            System.out.println("Hello, World!");
-        } else {
-            String names = String.join(", ", args);
-            System.out.println("Hello, " + names + "!");
+        computerMove();
+        printBoard();
+    }
+
+    static void computerMove() {
+        Random random = new Random();
+        int position;
+
+        do {
+            int slot = random.nextInt(9) + 1;
+            position = slot - 1;
+        } while (board[position] != ' ');
+
+        board[position] = 'O';
+    }
+
+    static void printBoard() {
+        for (int i = 0; i < board.length; i++) {
+            System.out.print(board[i]);
+            if ((i + 1) % 3 == 0) {
+                System.out.println();
+            } else {
+                System.out.print(" | ");
+            }
         }
     }
 }
