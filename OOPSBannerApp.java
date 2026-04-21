12e0e117cbece36b358ea7e4c064a70ebaa1655e
jagvijay singh <akshanshsingh2007@gmail.com>
2026-04-21 16:10:00 +0530

    Vuc3

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,13 +1,13 @@
+import java.util.Scanner;
+
 class OOPSBannerApp {
-    static int[] getRowCol(int slot) {
-        int row = (slot - 1) / 3;
-        int col = (slot - 1) % 3;
-        return new int[]{row, col};
+    static int getUserSlot() {
+        Scanner sc = new Scanner(System.in);
+        return sc.nextInt();
     }
 
     public static void main(String[] args) {
-        int slot = 7;
-        int[] result = getRowCol(slot);
-        System.out.println(result[0] + " " + result[1]);
+        int slot = getUserSlot();
+        System.out.println(slot);
     }
 }
