21aed424dc8c5af6a49c131a777393ef85d38369
jagvijay singh <akshanshsingh2007@gmail.com>
2026-04-21 15:59:13 +0530

    IVuc7

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -3,13 +3,7 @@ public class OOPSBannerApp {
         if (args.length == 0) {
             System.out.println("Hello, World!");
         } else {
-            StringBuilder nameBuilder = new StringBuilder();
-
-            for (String name : args) {
-                nameBuilder.append(name).append(", ");
-            }
-
-            String names = nameBuilder.substring(0, nameBuilder.length() - 2);
+            String names = String.join(", ", args);
             System.out.println("Hello, " + names + "!");
         }
     }
